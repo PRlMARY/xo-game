e144736d74bb51c2e8a0d8ac2b1b8a8b67a5a8c9
PRlMARY <pimmsam82@gmail.com>
2025-08-03 19:39:31 +0700

    init: project setup

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div className="text-3xl font-bold underline">
+            XnO GAME
+        </div>
+    );
+}
+
+export default Home;
